76eafcf72f863f575c091fd195e4fe7772a231d8
yosh <azuril5@hotmail.com>
2025-08-01 22:43:58 +0200

    Init

---
+++ src/main/java/org/yosh/testazure/TestAzureApplication.java
@@ -0,0 +1,13 @@
+package org.yosh.testazure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAzureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestAzureApplication.class, args);
+    }
+
+}
